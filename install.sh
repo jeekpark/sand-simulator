acae4340571e07c084a17df525fd85420467cf70
JEEKUN PARK <118240517+jeekpark@users.noreply.github.com>
2023-11-27 23:08:20 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,10 +11,8 @@ if [ "$arch" = "x86_64" ]; then
 elif [ "$arch" = "arm64" ]; then
   tar -xzvf ./sand-simulator/SFML-2.6.1-macOS-clang-arm64.tar.gz -C ./sand-simulator/sfml
   mv ./sand-simulator/sfml/SFML-2.6.1-macOS-clang-arm64 ./sand-simulator/sfml/2.6.1
-else
-  echo "Unsupported architecture: $arch"
 fi
 
-echo "Architecture: $arch"
+
 make -C sand-simulator clangd
 
