426d24c123db680da2cb00e3f0d8a924abdd395e
jeekpark <jeekpark@student.42seoul.kr>
2023-11-27 23:24:42 +0900

    fix: install.sh

--- install.sh
+++ install.sh
@@ -2,16 +2,21 @@
 
 git clone https://github.com/jeekpark/sand-simulator.git
 
+compiler=$(cc --version)
 arch=$(uname -m)
 
-mkdir -p ./sand-simulator/sfml
-if [ "$arch" = "x86_64" ]; then
-  tar -xzvf ./sand-simulator/SFML-2.6.1-macOS-clang-64-bit.tar.gz -C ./sand-simulator/sfml
-  mv ./sand-simulator/sfml/SFML-2.6.1-macOS-clang-64-bit ./sand-simulator/sfml/2.6.1
-elif [ "$arch" = "arm64" ]; then
-  tar -xzvf ./sand-simulator/SFML-2.6.1-macOS-clang-arm64.tar.gz -C ./sand-simulator/sfml
-  mv ./sand-simulator/sfml/SFML-2.6.1-macOS-clang-arm64 ./sand-simulator/sfml/2.6.1
+if [[ $compiler == *"gcc"* ]] && [ "$arch" = "x86_64" ]; then
+  sfml_bundle="SFML-2.6.1-linux-gcc-64-bit.tar.gz"
+elif [[ $compiler == *"clang"* ]] && [ "$arch" = "x86_64" ]; then
+  sfml_bundle="SFML-2.6.1-macOS-clang-64-bit.tar.gz"
+elif [[ $compiler == *"clang"* ]] && [ "$arch" = "arm64" ]; then 
+  sfml_bundle="SFML-2.6.1-macOS-clang-arm64.tar.gz"
 fi
+
+
+tar -xzvf ./sand-simulator/sfml/$sfml_bundle -C ./sand-simulator/sfml
+mv ./sand-simulator/sfml/$sfml_bundle ./sand-simulator/sfml/2.6.1
+
 
 
 make -C sand-simulator clangd
