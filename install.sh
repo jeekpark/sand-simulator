6602a9dfa2dd35843e6bf35fdc7c0ae65ad52448
JEEKUN PARK <118240517+jeekpark@users.noreply.github.com>
2023-11-27 22:59:51 +0900

    update: install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,18 @@
 
 git clone https://github.com/jeekpark/sand-simulator.git
 
+arch=$(uname -m)
+
 mkdir -p ./sand-simulator/sfml
-tar -xzvf ./sand-simulator/SFML-2.6.1-macOS-clang-arm64.tar.gz -C ./sand-simulator/sfml
-mv ./sand-simulator/sfml/SFML-2.6.1-macOS-clang-arm64 ./sand-simulator/sfml/2.6.1
+if [ "$arch" = "x86_64" ]; then
+  tar -xzvf ./sand-simulator/SFML-2.6.1-macOS-clang-64-bit.tar.gz -C ./sand-simulator/sfml
+  mv ./sand-simulator/sfml/SFML-2.6.1-macOS-clang-64-bit ./sand-simulator/sfml/2.6.1
+else if [ "$arch" = "arm64" ]; then
+  tar -xzvf ./sand-simulator/SFML-2.6.1-macOS-clang-arm64.tar.gz -C ./sand-simulator/sfml
+  mv ./sand-simulator/sfml/SFML-2.6.1-macOS-clang-arm64 ./sand-simulator/sfml/2.6.1
+else
+  echo "Unsupported architecture: $arch"
+fi
 
-
+echo "Architecture: $arch"
 make -C sand-simulator clangd
