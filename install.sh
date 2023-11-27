6023f197a8d70def650e0d895e83f5928f9f9ff3
JEEKUN PARK <118240517+jeekpark@users.noreply.github.com>
2023-11-27 22:41:39 +0900

    update: install.sh

--- install.sh
+++ install.sh
@@ -2,8 +2,9 @@
 
 git clone https://github.com/jeekpark/sand-simulator.git
 
-mkdir -p ./sand-simulator/sfml/2.6.1
+mkdir -p ./sand-simulator/sfml
+tar -xzvf ./sand-simulator/SFML-2.6.1-macOS-clang-arm64.tar.gz -C ./sand-simulator/sfml
+mv ./sand-simulator/sfml/SFML-2.6.1-macOS-clang-arm64 ./sand-simulator/sfml/2.6.1
 
-tar -xzvf ./sand-simulator/SFML-2.6.1-macOS-clang-arm64.tar.gz -C ./sand-simulator/sfml/2.6.1
 
 make -C sand-simulator clangd
